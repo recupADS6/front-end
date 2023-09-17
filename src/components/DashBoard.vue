36bc4fea2dc1c59d669f4586ef6d629490d158d0
vitoriasaturnino <vim.saturnino@gmail.com>
2023-09-17 20:58:54 -0300

    wip: implements dashboard page

---
+++ src/components/DashBoard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Dashboard</h1>
+    <p>Protected dashboard</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "DashBoard"
+  }
+</script>
