f716870c366f488fa282d46321029f34ae6af942
vitoriasaturnino <vim.saturnino@gmail.com>
2023-09-23 15:16:42 -0300

    feat: add jobs to cards

--- src/components/ListJobs.vue
+++ src/components/ListJobs.vue
@@ -6,25 +6,43 @@
 
     <n-tabs type="line" animated>
       <n-tab-pane name="jobs-open" tab="Abertas">
-        <div class="jobs-section">
-        <n-card class="jobs-card" title="Card" size="large">
-          Card Content
-        </n-card>
-      </div>
+        <n-space vertical>
+          <div v-for="job in jobsFilled" :key="job.id">
+            <n-card class="jobs-card" :title="job.title" size="large">
+              <n-space vertical>
+                <n-text><strong>Nível:</strong> {{ job.level }}</n-text>
+                <n-text><strong>Tipo:</strong> {{ job.type }}</n-text>
+                <n-text><strong>Categoria:</strong> {{ job.category }}</n-text>
+                <n-text><strong>Nível de Formação</strong> {{ job.scholarLevel }}</n-text>
+                <n-text><strong>CHA:</strong> {{ job.cha }}</n-text>
+                <n-text><strong>Descrição</strong> {{ job.description }}</n-text>
+              </n-space>
+            </n-card>
+          </div>
+        </n-space>
       </n-tab-pane>
-      <n-tab-pane name="complted-jobs" tab="Concluidas">
-        <div class="jobs-section">
-        <n-card class="jobs-card" title="Card" size="large">
-          Card Content
-        </n-card>
-      </div>
+      <n-tab-pane name="completed-jobs" tab="Concluídas">
+        <n-space vertical>
+          <div v-for="job in jobsOpen" :key="job.id">
+            <n-card class="jobs-card" :title="job.title" size="large">
+              <n-space vertical>
+                <n-text><strong>Nível:</strong> {{ job.level }}</n-text>
+                <n-text><strong>Tipo:</strong> {{ job.type }}</n-text>
+                <n-text><strong>Categoria:</strong> {{ job.category }}</n-text>
+                <n-text><strong>Nível de Formação</strong> {{ job.scholarLevel }}</n-text>
+                <n-text><strong>CHA:</strong> {{ job.cha }}</n-text>
+                <n-text><strong>Descrição</strong> {{ job.description }}</n-text>
+              </n-space>
+            </n-card>
+          </div>
+        </n-space>
       </n-tab-pane>
     </n-tabs>
   </div>
 </template>
 
 <script>
-  import { NH1, NCard } from 'naive-ui'
+  import { NH1, NCard, NText } from 'naive-ui'
   import { onMounted, ref } from 'vue'
   import { getAllJobs } from '../services/jobsService.js'
 
@@ -32,19 +50,34 @@
     name: "ListJobs",
     components: {
       NCard,
-      NH1
+      NH1,
+      NText
     },
 
     setup () {
       const jobsList = ref([]);
+      const jobsOpen = ref([]);
+      const jobsFilled = ref([]);
 
       onMounted(async () => {
         jobsList.value = await getAllJobs()
         console.log(jobsList.value.data)
+
+        jobsList.value.data.map (job => {
+          if (job.status === 'open') {
+            jobsOpen.value.push(job)
+          } else {
+            jobsFilled.value.push(job)
+          }
+        })
+        console.log(jobsOpen.value)
+        console.log(jobsFilled.value)
       });
 
       return {
         jobsList,
+        jobsOpen,
+        jobsFilled,
       }
     }
   };
@@ -57,10 +90,6 @@
     align-items: center;
     margin-top: 20px;
     width: 100%;
-  }
-
-  .jobs-card {
-    background-color: blueviolet;
   }
 
   .list-job-content {
