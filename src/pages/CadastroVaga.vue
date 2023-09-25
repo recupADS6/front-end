742cc8fc8bd9b435ededaaeab06fdbf9cbca32e3
Juliana Maria <juliana.ramos01@fatec.sp.gov.br>
2023-09-24 21:02:26 -0300

    fix: add CadastroVaga in pages

---
+++ src/pages/CadastroVaga.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <header-menu></header-menu>
+      <n-layout>
+        <n-layout-content content-style="padding: 24px;">
+          <div class="cadastro-vaga">
+            <h1>Cadastro de Vagas</h1>
+              <job-form></job-form>
+          </div>
+        </n-layout-content>
+      </n-layout>
+    </div>
+  </template>
+  
+  <script>
+  import HeaderMenu from '../components/HeaderMenu.vue';
+  import JobForm from '../components/JobForm.vue';
+  
+  export default {
+    components: {
+      HeaderMenu,
+      JobForm
+    },
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    text-align: center;
+  }
+  .n-layout-content {
+    background-color: #efefef ;
+    height: 90vh;
+  }
+  
+  </style>
